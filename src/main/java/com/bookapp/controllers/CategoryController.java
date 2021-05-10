8e48257a2dae986689a9c15d4205a0ae35d9ca1f
User <User@192.168.1.11>
2021-05-11 00:32:08 +0700

    update get categories

--- src/main/java/com/bookapp/controllers/CategoryController.java
+++ src/main/java/com/bookapp/controllers/CategoryController.java
@@ -2,20 +2,39 @@ package com.bookapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bookapp.controllers.Output.CategoryOutput;
+import com.bookapp.controllers.Output.CategoryFindAllOutput;
+import com.bookapp.controllers.Output.CategoryFindByIdOutput;
+import com.bookapp.services.Interfaces.IBookService;
 import com.bookapp.services.Interfaces.ICategoryService;
 
 @RestController
 public class CategoryController {
 	
 	@Autowired
-	private ICategoryService newService;
+	private ICategoryService categoryService;
+	@Autowired
+	private IBookService bookService;
+	
 	@GetMapping(value = "/categories")
-	public CategoryOutput showCategory() {
-		CategoryOutput result = new CategoryOutput();
-		result.setListResult(newService.findAll()); // test push
+	public CategoryFindAllOutput showCategoryByName(@RequestParam(value = "name", required = false) String name) {
+		CategoryFindAllOutput result = new CategoryFindAllOutput();
+		if(name != null) {
+			result.setListResult(categoryService.findByNameContaining(name));
+		} else {
+			result.setListResult(categoryService.findAll());
+		}
+		return result;
+	}
+	
+	@GetMapping(value = "/categories/{categoryId}")
+	public CategoryFindByIdOutput findById(@PathVariable("categoryId") int categoryId) {
+		CategoryFindByIdOutput result = new CategoryFindByIdOutput();
+		result.setBookCategoryDTO(categoryService.findById(categoryId));
+		result.setBookDTOs(bookService.findByCategoryId(categoryId));
 		return result;
 	}
 }
